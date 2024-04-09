a082ca882401977bee608d6ecacdcac03b7641fb
arleydemelquisedeclisbet <94724221+arleydemelquisedeclisbet@users.noreply.github.com>
2024-04-09 00:48:04 -0500

    2.20: Data for countries, step 3

--- Part2/data-for-countries/src/App.jsx
+++ Part2/data-for-countries/src/App.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react"
 import DetailCountry from "./components/DetailCountry"
+const apiKey = import.meta.env.VITE_WEATHER_API_KEY
 
 const App = () => {
 
   const [countries, setCountries] = useState([])
   const [countriesToShow, setCountriesToShow] = useState([])
+  const [weather, setWeather] = useState(null)
 
   useEffect(() => {
     fetch('https://studies.cs.helsinki.fi/restcountries/api/all')
@@ -23,15 +25,24 @@ const App = () => {
     setCountriesToShow([country])
   }
 
+  useEffect(() => {
+    if (countriesToShow.length === 1) {
+      const city = countriesToShow[0].capital[0]
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
+      .then(resp => resp.json())
+      .then(setWeather)
+    }
+  }, [countriesToShow])
+
   return (
     <>
-      <h2>Countries</h2>
+      <h2 style={{marginBottom: '0'}}>Countries</h2>
       <div>
         find countries <input onChange={handleChangeFilter} />
         {countriesToShow.length > 10
           ? <div>Too many matches, specify another filter</div>
           : countriesToShow.length === 1
-            ? <DetailCountry country={countriesToShow[0]} />
+            ? <DetailCountry country={countriesToShow[0]} weather={weather} />
             : <ul style={{ listStyleType: 'none', paddingLeft: 0 }}>
               {countriesToShow.map(country => (
                 <li key={country.cca3}>
--- Part2/data-for-countries/src/components/DetailCountry.jsx
+++ Part2/data-for-countries/src/components/DetailCountry.jsx
@@ -1,19 +1,29 @@
 
-const DetailCountry = ({country: { name, capital, area, languages, flag }}) => {
+const DetailCountry = ({ country: { name, capital, area, languages, flag }, weather }) => {
 
     return (
-        <>
-            <h1>{name.common}</h1>
-            <div>Capital {capital[0]}</div>
-            <div>Área {area}</div>
+        <section style={{display: 'flex', justifyContent: 'space-around', width: '50rem', background: '#b9b9b9 ', borderRadius: '50px', marginTop: '1em'}}>
+            <div>
+                <h1>{name.common}</h1>
+                <div>Capital {capital[0]}</div>
+                <div>Área {area}</div>
 
-            <h3>Languages:</h3>
-            <ul style={{marginBottom: '0'}}>
-                { Object.entries(languages).map(([key, value]) => <li key={key}>{value}</li>) }
-            </ul>
+                <h3>Languages:</h3>
+                <ul style={{marginBottom: '0'}}>
+                    { Object.entries(languages).map(([key, value]) => <li key={key}>{value}</li>) }
+                </ul>
 
-            <div style={{ fontSize: '15rem' }}>{flag}</div>
-        </>
+            </div>
+            <div style={{ fontSize: '15rem', padding: '0' }}>{flag}</div>
+            {weather !== null && 
+                <div>
+                    <h1>Weather in {capital[0]}</h1>
+                    <div>Temperature: {weather.main.temp} Celcius</div>
+                    <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} width="140" alt={`Img from ${capital[0]}`} />
+                    <div>Wind {weather.wind.speed} m/s</div>
+                </div>
+            }
+        </section>
     )
 }
 
